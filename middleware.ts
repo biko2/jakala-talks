120c0b743493661310bc798d4c94f0c1371df050
drzkn <diegorazq@gmail.com>
2025-09-25 21:42:56 +0200

    simplifies middleware

--- middleware.ts
+++ middleware.ts
@@ -33,11 +33,10 @@ export async function middleware(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    request.nextUrl.pathname !== '/'
   ) {
     const url = request.nextUrl.clone()
-    url.pathname = '/login'
+    url.pathname = '/'
     return NextResponse.redirect(url)
   }
 
