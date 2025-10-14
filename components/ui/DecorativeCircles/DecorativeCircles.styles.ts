8adef6b23bda014eee7e1cca4ddb285d320f619d
drzkn <diegorazq@gmail.com>
2025-10-14 19:55:41 +0200

    adds box shadow

--- components/ui/DecorativeCircles/DecorativeCircles.styles.ts
+++ components/ui/DecorativeCircles/DecorativeCircles.styles.ts
@@ -5,6 +5,9 @@ export const Circle = styled.div<{
   $right?: number
   $bottom?: number
 }>`
+  -webkit-box-shadow: 1px 1px 5px 1px rgba(0,0,0,0.47); 
+  box-shadow: 1px 1px 5px 1px rgba(0,0,0,0.47);
+
   position: absolute;
   width: ${props => props.$size}px;
   height: ${props => props.$size}px;
