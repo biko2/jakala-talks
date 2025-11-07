0b76cdf0e569f41a55f9cfbf0a9419daab9e40b4
drzkn <diegorazq@gmail.com>
2025-11-07 08:39:58 +0100

    feat: fix logic

--- components/talks/TalksList/TalksList.tsx
+++ components/talks/TalksList/TalksList.tsx
@@ -36,7 +36,7 @@ export default function TalksList({
 
   return (
     <Container>
-      {isVotingEnabled && !isLoggedIn && (
+      {(!isVotingEnabled || isLoggedIn) && (
         <VotingStatus>
           {getVotingMessage()}
         </VotingStatus>
